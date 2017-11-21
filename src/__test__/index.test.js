ecc2b0b9fa0a82769f7ffb44fe87421570e4737d
Jurij <yurastein@gmail.com>
2017-11-21 08:43:15 +0200

    processArgs test

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -3,21 +3,23 @@ const processArgs = require('../processArgs');
 describe('processArgs', () => {
   test('new args', () => {
     const args = processArgs([
+      'porcess path',
+      'process name',
       '--api=test',
       '--mocks=/test/test',
       '--port=1234',
-      '--socketPort=1235',
+      '--socketport=1235',
       '--config=/test/test',
       '--https=true',
       '--log=false',
       '--origin=https://test.com',
       '--referer=https://test.com',
     ]);
-    expect(args).toContainEqual({
+    expect(args).toEqual({
       '--api': 'test',
       '--mocks': '/test/test',
       '--port': '1234',
-      '--socketPort': '1235',
+      '--socketport': '1235',
       '--config': '/test/test',
       '--https': 'true',
       '--log': 'false',
@@ -27,7 +29,7 @@ describe('processArgs', () => {
         '--api': 'https://localhost',
         '--mocks': '',
         '--port': '1234',
-        '--socketPort': '1235',
+        '--socketport': '1235',
         '--config': '../dev-server-config.json',
         '--https': false,
         '--log': true,
